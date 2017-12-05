df73096f43384184fc5d37e98f6ae9efbfdd51db
Rodolfo Peixoto <rodolfopeixoto@campos.rj.gov.br>
2017-12-05 13:34:42 -0200

    Refactory Gear add Struct to Wheel

--- gear.rb
+++ gear.rb
@@ -2,27 +2,30 @@ class Gear
   attr_reader :chainring, :cog, :rim, :tire
 
   def initialize(chainring, cog, rim, tire)
-    @chainring = chainring
-    @cog = cog
-    @rim = rim
-    @tire = tire
-  end
-
-  def gear_inches
-    # tire goes around rim twice for diameter
-    ratio * diameter
+    @chainring  = chainring
+    @cog        = cog
+    @wheel      = Wheel.new(rim,tire) 
   end
 
   def ratio
     chainring / cog.to_f
   end
 
-  def diameter
-    rim + (tire * 2)
+  def gear_inches
+    # tire goes around rim twice for diameter
+    ratio * diameter
   end
+  
+  Wheel = Struct.new(:rim, :tire) do
+    def diameter
+      rim + (tire * 2)
+    end
+  end
+end
+
+
 
 #   puts Gear.new(52, 11).ratio
 #   puts Gear.new(30, 27).ratio
-  puts Gear.new(52,11,26,1.5).gear_inches
-  puts Gear.new(52,11,24, 1.25).gear_inches
-end
+puts Gear.new(52,11,26,1.5).gear_inches
+puts Gear.new(52,11,24, 1.25).gear_inches
