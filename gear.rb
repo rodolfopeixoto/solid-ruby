30b52522db27f45ae095763e4cb4ddd554ac262e
Rodolfo Peixoto <rodolfopeixoto@campos.rj.gov.br>
2017-12-05 14:00:19 -0200

    Refactory create class wheel and gear

--- gear.rb
+++ gear.rb
@@ -1,10 +1,12 @@
-class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+require_relative 'wheel'
 
-  def initialize(chainring, cog, rim, tire)
+class Gear
+  attr_reader :chainring, :cog, :wheel
+
+  def initialize(chainring, cog, wheel=nil)
     @chainring  = chainring
     @cog        = cog
-    @wheel      = Wheel.new(rim,tire) 
+    @wheel      = wheel 
   end
 
   def ratio
@@ -13,13 +15,7 @@ class Gear
 
   def gear_inches
     # tire goes around rim twice for diameter
-    ratio * diameter
-  end
-  
-  Wheel = Struct.new(:rim, :tire) do
-    def diameter
-      rim + (tire * 2)
-    end
+    ratio * wheel.diameter
   end
 end
 
@@ -27,5 +23,7 @@ end
 
 #   puts Gear.new(52, 11).ratio
 #   puts Gear.new(30, 27).ratio
-puts Gear.new(52,11,26,1.5).gear_inches
-puts Gear.new(52,11,24, 1.25).gear_inches
+@wheel = Wheel.new(26,1.5)
+puts "Wheel: #{@wheel.circumference}"
+puts Gear.new(52,11,@wheel).gear_inches
+puts Gear.new(52,11).ratio
